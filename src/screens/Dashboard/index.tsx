7be86a878c82ad59d978eda5cfd052b313e3a8cf
Rafael Tozzi <rafael.tozzi@hotmail.com>
2021-08-24 09:17:03 -0300

    Initialize

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+import * as styles from "./styles";
+
+export function Dashboard() {
+  return (
+    <styles.Container>
+      <Text>Dashboard</Text>
+    </styles.Container>
+  );
+}
