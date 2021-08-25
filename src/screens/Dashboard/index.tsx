c8ad519d2c785624d7a6c2dbb50572404daf4d1c
Rafael Tozzi <rafael.tozzi@hotmail.com>
2021-08-25 01:31:53 -0300

    .

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
 
 import * as styles from "./styles";
 
 export function Dashboard() {
   return (
     <styles.Container>
-      <Text>Dashboard</Text>
+      <styles.Header>
+        <styles.UserWrapper>
+          <styles.UserInfo>
+            <styles.Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/44178301?v=4",
+              }}
+            />
+            <styles.User>
+              <styles.UserGreeting>Olá,</styles.UserGreeting>
+              <styles.UserName>Rafael</styles.UserName>
+            </styles.User>
+          </styles.UserInfo>
+        </styles.UserWrapper>
+      </styles.Header>
     </styles.Container>
   );
 }
